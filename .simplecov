85ef9f87547490eb44dadaf73e4716aeac10687e
Joel Sommers <jsommers@colgate.edu>
2019-05-07 09:27:20 -0400

    add omniauth tests; coverage 100\%

--- .simplecov
+++
@@ -1,4 +0,0 @@
-SimpleCov.start 'rails' do
-  add_filter '/spec/'
-  add_filter '/app/controllers/users/'
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,31 @@ RSpec.describe User, type: :model do
   it "should disallow creation with no email" do
     expect{ User.create! }.to raise_error(ActiveRecord::RecordInvalid)
   end
+
+  describe "from_omniauth" do
+    it "should return nil if email is nil" do
+      o = double('oauth')
+      d = double('data')
+      expect(d).to receive(:[]).with("email") { nil }
+      expect(o).to receive(:info) { d }
+      expect(User.from_omniauth(o)).to be nil
+    end
+
+    it "should return nil if the email is there but user doesn't exist" do
+      o = double('oauth')
+      d = double('data')
+      expect(d).to receive(:[]).with("email") { "jsommers@acm.org" }
+      expect(o).to receive(:info) { d }
+      expect(User.from_omniauth(o)).to be nil
+    end
+
+    it "should return the email address if the user exists" do
+      u = User.create!(:email => "jsommers@acm.org")
+      o = double('oauth')
+      d = double('data')
+      expect(d).to receive(:[]).with("email") { "jsommers@acm.org" }
+      expect(o).to receive(:info) { d }
+      expect(User.from_omniauth(o)).to eq(u)
+    end
+  end
 end
