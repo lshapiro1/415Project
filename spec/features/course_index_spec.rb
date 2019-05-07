372f54eaa8a77d37bd610e7a754dbe1e8ca22ed4
Joel Sommers <jsommers@colgate.edu>
2019-05-07 10:58:24 -0400

    add courses index

--- spec/features/course_index_spec.rb
+++ spec/features/course_index_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "CourseIndices", type: :feature do
+  include Devise::Test::IntegrationHelpers
+
   describe "course index page" do
     it "should list courses that exist" do
+      sign_in FactoryBot.create(:user)
       Course.create!(:name => "One", :daytime => "TR 8:30-9:55")
       Course.create!(:name => "Two", :daytime => "MWF 10:20-11:10")
 
       visit courses_path
-      expect(page).to contain("One")
-      expect(page).to contain("Two")
+      expect(page.text).to match(/student@colgate.edu/)
+      expect(page.text).to match(/One.+TR 8:30-9:55/)
+      expect(page.text).to match(/Two.+MWF 10:20-11:10/)
     end
   end
 end
