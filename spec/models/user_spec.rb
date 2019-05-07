0b9ccfd1be5b0072e7dc3a4263ba9f47785e699c
Joel Sommers <jsommers@colgate.edu>
2019-05-07 08:25:05 -0400

    model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "should disallow creation with no email" do
-      expect{ User.create! }.to raise_error(ActiveRecord::RecordInvalid)
+    expect{ User.create! }.to raise_error(ActiveRecord::RecordInvalid)
   end
 end
