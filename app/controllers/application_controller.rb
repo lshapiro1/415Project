d6042f4bd0726c2df1734c616cd150613fbb9145
Joel Sommers <jsommers@colgate.edu>
2019-05-10 06:28:16 -0400

    refactor invalid id handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  rescue_from ActiveRecord::RecordNotFound, :with => :invalid_id
 
 protected
   def redirect_if_student
     redirect_to courses_path if current_user.student?
+  end
+
+  def invalid_id
+    flash[:warning] = "Invalid identifier"
+    redirect_to courses_path
   end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,20 +4,15 @@ class CoursesController < ApplicationController
   end
 
   def show
-    begin
-      @course = Course.find(params[:id])
-      if current_user.student? 
-        if !current_user.courses.include? @course
-          flash[:notice] = "You're not enrolled in #{@course.name}"
-          redirect_to courses_path and return
-        end
-        render 'show_student'
-      else
-        render 'show_admin'        
+    @course = Course.find(params[:id])
+    if current_user.student? 
+      if !current_user.courses.include? @course
+        flash[:notice] = "You're not enrolled in #{@course.name}"
+        redirect_to courses_path and return
       end
-    rescue ActiveRecord::RecordNotFound
-      flash[:warning] = "Invalid course"
-      redirect_to courses_path and return
+      render 'show_student'
+    else
+      render 'show_admin'        
     end
   end
 end
