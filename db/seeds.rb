5a0e6e9d1bb7e38ccd70f40f82d9d63b77f31a7a
Joel Sommers <jsommers@colgate.edu>
2019-05-13 19:51:54 -0400

    update seeds, start poll activate feature spec

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(:email => 'jsommers@colgate.edu', :admin => true)
+User.create!(:email => 'sommersmeister@gmail.com')
+Course.create!(:name => 'COSC 101A', :daytime => 'TR 8:30-9:45')
