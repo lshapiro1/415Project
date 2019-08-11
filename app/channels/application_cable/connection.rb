acbca6f1b0d6e1b38711cd86da4d1b58097f71fa
Joel Sommers <jsommers@colgate.edu>
2019-08-11 09:58:02 -0400

    fix syntax in action cable connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,11 +10,10 @@ module ApplicationCable
     protected
       def find_verified_user
         verified_user = User.find_by(id: cookies.signed['user.id'])
-          if verified_user && cookies.signed['user.expires_at'] > Time.now
-            verified_user
-          else
-            reject_unauthorized_connection
-          end
+        if verified_user && cookies.signed['user.expires_at'] > Time.now
+          verified_user
+        else
+          reject_unauthorized_connection
         end
       end
   end
