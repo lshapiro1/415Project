5442711e29cae1b24e7b5ab42550735201d628f9
Joel Sommers <jsommers@colgate.edu>
2019-08-11 18:41:50 -0400

    tweak seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,9 @@
 #
 User.create!(:email => 'jsommers@colgate.edu', :admin => true)
 u = User.create!(:email => 'sommersmeister@gmail.com')
-c = Course.create!(:name => 'COSC 101D', :daytime => 'TR 8:30-9:45')
+c = Course.create!(:name => 'COSC101F19', :daytime => 'TR 8:30-9:45')
 c.students << u
+
 
 Question.new # force loading model code
 questions = []
@@ -43,4 +44,4 @@ slist = []
   r.save!
 end
 
-c2 = Course.create!(:name => 'COSC 301A', :daytime => 'TR 9:55-11:10')
+c2 = Course.create!(:name => 'COSC301F19', :daytime => 'TR 9:55-11:10')
